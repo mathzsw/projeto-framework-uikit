d35351ef4016a958e214135f73ec0af326afd7bb
Matheus Augusto Rodrigues de Alencar <mara@discente.ifpe.edu.br>
2026-09-16 10:30:23 -0300

    Correção do pacote inicial

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+
 const sequelize = require('./config/bd');
-const Filme = require("./models/filme.model");
-const FichaTecnica = require("./models/fichaTecnica.model");
+const Filme = require('./models/filme.model');
+const Artista = require('./models/artista.model');
+const Diretor = require('./models/diretor.model');
+const FichaTecnica = require('./models/fichaTecnica.model');
+
 require("./models/relacionamentosModels.model");
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/artistas', async (req, res) => {
   const artistas = await Artista.findAll({ raw: true });
-  res.render('artistas', { artistas });
+  res.render('artistas/artista', { artistas });
 });
 
 app.get('/artistas/cadastrar', (req, res) => {
-  res.render('cadastrarArtista');
+  res.render('artistas/cadastrarArtista');
 });
 
 app.post('/artistas', async (req, res) => {
@@ -33,7 +47,7 @@ app.get('/artistas/:id', async (req, res) => {
     include: [{ model: Filme, as: 'filmes' }]
   });
 
-  res.render('detalharArtista', { artista: artista.toJSON() });
+  res.render('artistas/detalharArtista', { artista: artista.toJSON() });
 });
 
 
@@ -42,7 +56,7 @@ app.get('/filmes/:id/ficha-tecnica/cadastrar', async (req, res) => {
 
   const filme = await Filme.findByPk(id, { raw: true });
 
-  res.render('cadastrarFichaTecnica', { filme });
+  res.render('fichasTecnicas/cadastrarFichaTecnica', { filme });
 });
 
 app.post('/filmes/:id/ficha-tecnica', async (req, res) => {
@@ -62,61 +76,38 @@ app.post('/filmes/:id/ficha-tecnica', async (req, res) => {
   res.redirect(`/filmes/${id}`);
 });
 
-app.get('/filmes/:id', async (req, res) => {
-  const id = req.params.id;
+app.get('/filmes', async (req, res) => {
+  const filmes = await Filme.findAll({ raw: true });
 
-  const filme = await Filme.findByPk(id, {
-    include: [{ model: FichaTecnica, as: 'fichaTecnica' }]
+  res.render('filmes/filme', { filmes });
+});
+
+
+app.get('/filmes/cadastrar', async (req, res) => {
+  const artistas = await Artista.findAll({ raw: true });
+  const diretores = await Diretor.findAll({ raw: true });
+
+  res.render('filmes/cadastrarFilme', {
+    artistas,
+    diretores
   });
-
-  res.render('detalharFilme', { filme: filme.toJSON() });
 });
 
 app.post('/filmes', async (req, res) => {
-  const nome = req.body.nome;
+  const titulo = req.body.titulo;
   const ano = req.body.ano;
   const diretorId = req.body.diretorId;
+  const artistas = req.body.artistas;
 
-  await Filme.create({
-    nome: nome,
+  const filme = await Filme.create({
+    titulo: titulo,
     ano: ano,
     diretorId: diretorId
   });
 
-  res.redirect('/filmes');
-});
-
-app.get('/filmes/cadastrar', async (req, res) => {
-  const artistas = await Artista.findAll({ raw: true });
-  res.render('cadastrarFilme', { artistas });
-});
-
-app.get('/filmes/cadastrar', async (req, res) => {
-  const diretores = await Diretor.findAll({ raw: true });
-  res.render('cadastrarFilme', { diretores });
-});
-
-app.get('/filmes/:id', async (req, res) => {
-  const id = req.params.id;
-
-  const filme = await Filme.findByPk(id, {
-    include: [{ model: Diretor, as: 'diretor' }]
-  });
-
-  res.render('detalharFilme', { filme: filme.toJSON() });
-});
-
-app.post('/filmes', async (req, res) => {
-  const nome = req.body.nome;
-  const ano = req.body.ano;
-  const artistas = req.body.artistas; // array com os ids selecionados
-
-  const filme = await Filme.create({
-    nome: nome,
-    ano: ano
-  });
-
-  await filme.setArtistas(artistas);
+  if (artistas) {
+    await filme.setArtistas(artistas);
+  }
 
   res.redirect('/filmes');
 });
@@ -125,19 +116,28 @@ app.get('/filmes/:id', async (req, res) => {
   const id = req.params.id;
 
   const filme = await Filme.findByPk(id, {
-    include: [{ model: Artista, as: 'artistas' }]
+    include: [
+      { model: FichaTecnica, as: 'fichaTecnica' },
+      { model: Diretor, as: 'diretor' },
+      { model: Artista, as: 'artistas' }
+    ]
   });
 
-  res.render('detalharFilme', { filme: filme.toJSON() });
-});
+  if (!filme) {
+    return res.status(404).send('Filme não encontrado');
+  }
 
+  res.render('filmes/detalharFilme', {
+    filme: filme.toJSON()
+  });
+});
 app.get('/diretores', async (req, res) => {
   const diretores = await Diretor.findAll({ raw: true });
-  res.render('diretores', { diretores });
+  res.render('diretores/diretor', { diretores });
 });
 
 app.get('/diretores/cadastrar', (req, res) => {
-  res.render('cadastrarDiretor');
+  res.render('diretores/cadastrarDiretor');
 });
 
 app.post('/diretores', async (req, res) => {
@@ -161,5 +161,15 @@ app.get('/diretores/:id', async (req, res) => {
     include: [{ model: Filme, as: 'filmes' }]
   });
 
-  res.render('detalharDiretor', { diretor: diretor.toJSON() });
+  res.render('diretores/detalharDiretor', { diretor: diretor.toJSON() });
 });
+
+sequelize.sync()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Servidor rodando em http://localhost:3000');
+    });
+  })
+  .catch((erro) => {
+    console.log('Erro ao conectar com o banco:', erro);
+  });
